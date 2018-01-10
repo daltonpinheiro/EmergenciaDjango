2544557f808ea7c456ef83498ba64e7bdf48d434
Dalton Santos Pinheiro <Dalton.Pinheiro@ebserh.gov.br>
2018-01-10 11:34:58 -0300

    Sistema em produção, arquivos estáticos mudados

--- manchester/settings.py
+++ manchester/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hgre5wmgy#!9chwl_r-5*lbl%j%%wtbp3!7aqsflh5tpxv@qn+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     #libs
     'widget_tweaks',
     'import_export',
+    'whitenoise',
     #apps
     'formulario',
 ]
@@ -119,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Campo_Grande'
 
 USE_I18N = True
 
@@ -132,3 +133,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
